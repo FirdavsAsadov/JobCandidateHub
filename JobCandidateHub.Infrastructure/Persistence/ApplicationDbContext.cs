aaea3af92beeb02480afed5f48eeac93ae8235fb
Firdavs Asadov <toshmurodovj13@gmail.com>
2025-03-27 20:36:09 +0500

    remove default controler and add ApplicationDbContext

--- JobCandidateHub.Infrastructure/Persistence/ApplicationDbContext.cs
+++ JobCandidateHub.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,21 @@
-﻿namespace JobCandidateHub.Infrastructure.Persistence;
-public class ApplicationDbContext
+﻿using JobCandidateHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobCandidateHub.Infrastructure.Persistence;
+public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Candidate> Candidates { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Candidate>()
+            .HasIndex(c => c.Email)
+            .IsUnique(); 
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
